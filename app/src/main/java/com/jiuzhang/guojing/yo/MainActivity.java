1f5953623d27e22f330d8c2f490d0c06012b5b3d
chengzheng-wang <cwang92@binghamton.edu>
2016-11-19 17:09:06 -0500

    finish with animation

--- app/src/main/java/com/jiuzhang/guojing/yo/MainActivity.java
+++ app/src/main/java/com/jiuzhang/guojing/yo/MainActivity.java
@@ -3,6 +3,8 @@ package com.jiuzhang.guojing.yo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,35 +16,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        final Animation animation = AnimationUtils.loadAnimation(this, R.anim.text_anim);
         final EditText editText = (EditText)findViewById(R.id.name);
         // TODO set up Buttons
-        Button yo = (Button)findViewById(R.id.btn_1);
-        Button mo = (Button)findViewById(R.id.btn_2);
-        Button yoyo = (Button)findViewById(R.id.btn_3);
-        Button or = (Button)findViewById(R.id.btn_4);
+        final Button yo = (Button)findViewById(R.id.btn_1);
+        final Button mo = (Button)findViewById(R.id.btn_2);
+        final Button yoyo = (Button)findViewById(R.id.btn_3);
+        final Button or = (Button)findViewById(R.id.btn_4);
         yo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"YO" + editText.getText(),Toast.LENGTH_SHORT).show();
+                yo.setAnimation(animation);
+                Toast.makeText(MainActivity.this,yo.getText() +" "+ editText.getText(),Toast.LENGTH_SHORT).show();
             }
         });
         mo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"MO" + editText.getText(),Toast.LENGTH_SHORT).show();
+                mo.setAnimation(animation);
+                Toast.makeText(MainActivity.this,mo.getText() + " " + editText.getText(),Toast.LENGTH_SHORT).show();
             }
         });
         yoyo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"YO YO" + editText.getText(),Toast.LENGTH_SHORT).show();
+                yoyo.setAnimation(animation);
+                Toast.makeText(MainActivity.this,yoyo.getText() + " " + editText.getText(),Toast.LENGTH_SHORT).show();
             }
         });
         or.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"OR" + editText.getText(),Toast.LENGTH_SHORT).show();
+                or.setAnimation(animation);
+                Toast.makeText(MainActivity.this,or.getText() + " " + editText.getText(),Toast.LENGTH_SHORT).show();
             }
         });
         // find the Buttons you declared in the layout file by their ids
